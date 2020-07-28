df443d69f78e07b5f0a1686b6c64c755f58c6621
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-07-28 13:37:54 +0800

    Update spectate.js

--- game/spectate.js
+++ game/spectate.js
@@ -8,10 +8,13 @@ module.exports = {
   run: async (bot, message, args) => {
     if (message.guild.id != "729900329403154463") return;
     let status = db.fetch(`status`);
-    if (status != "hosted") return;
+    if (status === "ended") return;
     if (message.member.roles.cache.has("729900329659007074")) {
       message.member.roles.remove("729900329659007074").catch(console.error);
       message.member.roles.add("729900329659007070").catch(console.error);
+      message.channel.send(
+        `<@${message.author.id}>, please also ping the Host to remove your number role.`
+      );
     } else {
       message.member.roles.add("729900329659007070").catch(console.error);
     }
