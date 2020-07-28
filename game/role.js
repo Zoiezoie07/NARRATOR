bccc5e91379f29ee86e3103488fa29c3c3ab8d65
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-07-28 13:58:53 +0800

    Create role.js

---
+++ game/role.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+module.exports = {
+  name: "spectate",
+  description: "to spectate a game",
+  catgory: "game",
+  run: async (bot, message, args) => {}
+};
