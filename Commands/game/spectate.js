cf1dc5e6effca0c1d095e437f01ccc764a216ebb
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-07-28 12:44:52 +0800

    Update spectate.js

--- Commands/game/spectate.js
+++ Commands/game/spectate.js
@@ -9,11 +9,11 @@ module.exports = {
     if (message.guild.id != "729900329403154463") return;
     let status = db.fetch(`status`);
     if (status != "hosted") return;
-    if (message.member.roles.cache.has("729900329659007070")) {
-      message.member.roles.remove("729900329659007070").catch(console.error);
-      message.member.roles.add("729900329659007074").catch(console.error);
+    if (message.member.roles.cache.has("729900329659007074")) {
+      message.member.roles.remove("729900329659007074").catch(console.error);
+      message.member.roles.add("729900329659007070").catch(console.error);
     } else {
-      message.member.roles.add("729900329659007074").catch(console.error);
+      message.member.roles.add("729900329659007070").catch(console.error);
     }
   }
 };
