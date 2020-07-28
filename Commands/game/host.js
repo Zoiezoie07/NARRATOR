20c1c7bc11e8925bf072ad04e1b856965cef2e11
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-07-28 13:08:32 +0800

    Update host.js

--- Commands/game/host.js
+++ Commands/game/host.js
@@ -1,4 +1,4 @@
-const {MessageEmbed, Discord} = require("discord.js");
+const { MessageEmbed, Discord } = require("discord.js");
 const db = require("quick.db");
 
 module.exports = {
@@ -6,14 +6,17 @@ module.exports = {
   description: "host",
   catgory: "game",
   run: async (bot, message, args) => {
+    if (message.channel.id != "729900329944088600") return;
     let link = db.fetch(`link`);
     let achannel = message.guild.channels.cache.get("728044277313306716");
-    achannel.send(
-      `<@&728035352098504736>`);
-    const _ = new MessageEmbed()
-    .setTitle('New game starting!')
-    .setColor('RANDOM')
-    .setDescription(`Game Host: <@${message.author.id}>\nInvite Link: ${link}`);
-    achannel.send(_)
+    const embed = new MessageEmbed()
+      .setTitle("New game starting!")
+      .setColor("RANDOM")
+      .setDescription(
+        `Game Host: <@${message.author.id}>\nInvite Link: ${link}`
+      );
+    achannel.send(`<@&728035352098504736>`);
+    achannel.send(embed);
+    db.set(`status`, `hosted`);
   }
-}
+};
